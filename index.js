cb8e63102496788a5a1a420817e22f956b8da9b9
uddhab-choudhury <uddhab.choudhury@gmail.com>
2026-09-15 18:21:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // document.querySelector("button").addEventListener("click", handleClick) //here, target is the "button". Target specifies where the evenet is taking place, in our case the event is the click of a button.
-// //if you notice, above the function handleClick is called without (). To find out why, loo at the undemy course video at est. time stamp 6:00 minutes.
+// //if you notice, above the function handleClick is called without (). To find out why, look at the undemy course video at est. time stamp 6:00 minutes.
 
 // function handleClick() {
 //     alert("I got clicked!");
 // }
 
 //The same above we can write below using an 'anonymous function' and further adding a for loop for all the buttons:
+
+//Detecting button press:-
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (i = 0; i < numberOfDrumButtons; i++) {
@@ -20,7 +22,69 @@ document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
     var buttonInnerHTML = this.innerHTML;
 
-    switch(buttonInnerHTML) {
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+    // switch(buttonInnerHTML) {
+    //     case "w":
+    //         var tom1 = new Audio("sounds/tom-1.mp3");
+    //         tom1.play();
+    //       break;
+
+    //     case "a":
+    //         var tom2 = new Audio("sounds/tom-2.mp3");
+    //         tom2.play();
+    //       break;
+
+    //     case "s":
+    //         var tom3 = new Audio("sounds/tom-3.mp3");
+    //         tom3.play();
+    //       break;
+
+    //     case "d":
+    //         var tom4 = new Audio("sounds/tom-4.mp3");
+    //         tom4.play();
+    //       break;
+
+    //     case "j":
+    //         var crash = new Audio("sounds/crash.mp3");
+    //         crash.play();
+    //       break;
+
+    //     case "k":
+    //         var kick = new Audio("sounds/kick-bass.mp3");
+    //         kick.play();
+    //       break;
+
+    //     case "l":
+    //         var snare = new Audio("sounds/snare.mp3");
+    //         snare.play();
+    //       break;
+
+    //     default: console.log();
+
+    // }
+        
+
+
+
+}) //here, target is the "button". Target specifies where the evenet is taking place, in our case the event is the click of a button.
+//if you notice, above the function handleClick is called without (). To find out why, loo at the undemy course video at est. time stamp 6:00 minutes.
+}
+
+//Detecting keyboard press:-
+
+document.addEventListener("keydown", function(event){ //here, entire documents listens for any key press
+    console.log(event);
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+
+    switch(key) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
@@ -59,17 +123,16 @@ document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         default: console.log();
 
     }
-        
-
-
-
-}) //here, target is the "button". Target specifies where the evenet is taking place, in our case the event is the click of a button.
-//if you notice, above the function handleClick is called without (). To find out why, loo at the undemy course video at est. time stamp 6:00 minutes.
 }
 
-
-
-
+function buttonAnimation(currentKey) {
+ var activeButton = document.querySelector("." + currentKey);
+ activeButton.classList.add("pressed"); //for this, look up "pressed" class in styles.css
+ 
+ setTimeout(function() {
+  activeButton.classList.remove("pressed")}, 100);
+  
+}
 
 
 
